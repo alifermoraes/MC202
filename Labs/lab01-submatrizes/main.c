189d1e3806619503fec66b357e2d3ce81fd9eec1
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-15 17:36:25 -0300

    Change some identation in lab01-submatrizes/main.c

--- Labs/lab01-submatrizes/main.c
+++ Labs/lab01-submatrizes/main.c
@@ -7,16 +7,19 @@
  * Trabalho: 01-sumatriz
  * 
  * Submatriz
- * Dada uma matriz de inteiros positivos n por m, estamos interessados em encontrar a maior soma de
+ * Dada uma matriz de inteiros positivos n por m, estamos interessados em
+ * encontrar a maior soma de
  * elementos possível em uma submatriz de tamanho r por s.
  *
  * Entrada
- * A entrada tem uma linha com inteiros n, m, r, s separados por espaços, sendo que 1≤n≤1000,
- * 1≤m≤1000, 1≤r≤n e 1≤s≤m. Depois vêm n linhas com m inteiros cada, com os números em cada linha
+ * A entrada tem uma linha com inteiros n, m, r, s separados por espaços, sendo
+ * que 1≤n≤1000, 1≤m≤1000, 1≤r≤n e 1≤s≤m. Depois vêm n linhas com m inteiros
+ * cada, com os números em cada linha
  * e coluna da matriz.
  *
  * Saída
- * O programa deve imprimir o valor da maior soma possível em uma submatriz de tamanho r por s em
+ * O programa deve imprimir o valor da maior soma possível em uma submatriz de
+ * tamanho r por s em
  * uma única linha.
  */
 
@@ -41,11 +44,13 @@ int main(void) {
     }
 
     /* Calcula as somas das submatrizes e retorna o maior valor obtido */
-    for (i = 0; i <= lines - sub_lines; i++) { /* Laço para percorrer a matriz original e avaliar cada submatriz possivel */
+    /* O laço de i,j avalia todas as submatrizes possíveis */
+    for (i = 0; i <= lines - sub_lines; i++) { 
         for (j = 0; j <= columns - sub_columns; j++) {
             sum = 0;
 
-            for (k = i; k < i + sub_lines; k++) { /* Laço para percorrer e somar os valores das submatrizes */
+            /* O laço k,l soma todos os elementos de cada submatriz */
+            for (k = i; k < i + sub_lines; k++) { 
                 for (l = j; l < j + sub_columns; l++) {
                     sum += M[k][l];
                 }
