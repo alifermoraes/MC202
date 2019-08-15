6a6654d05bd760a1df124e9d03d710a307b68c03
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-15 17:30:54 -0300

    Format lab50-ascii/main.c properly

--- Labs/lab50-ascii/main.c
+++ Labs/lab50-ascii/main.c
@@ -8,9 +8,13 @@
  * 
  * Tabela ASCII
  *
- * A tabela ASCII é uma das formas possíveis de mapear inteiros em símbolos. São 128 códigos, de 0 a 127, sendo que os primeiros 32 e o último são considerados símbolos de controle e os demais são imprimíveis.
+ * A tabela ASCII é uma das formas possíveis de mapear inteiros em símbolos.
+ * São 128 códigos, de 0 a 127, sendo que os primeiros 32 e o último são
+ * considerados símbolos de controle e os demais são imprimíveis.
  *
- * Escreva um programa em C para imprimir uma tabela ASCII como a do modelo abaixo. No exemplo algumas linhas foram suprimidas e estão representadas por três  pontos. O programa não recebe nenhuma entrada.
+ * Escreva um programa em C para imprimir uma tabela ASCII como a do modelo
+ * abaixo. No exemplo algumas linhas foram suprimidas e estão representadas por
+ * três  pontos. O programa não recebe nenhuma entrada.
 */
 
 #include <stdio.h>
@@ -23,31 +27,29 @@ int main(void) {
     printf("    +------------------------ \n");
 
     printf("%3d | ", c);
-    for (i = 1; i <= 8; i++) {
-        printf("%2c ", c);
-        c++;
+    for (i = 1; i < 9; i++) {
+        printf("%2c ", c++);
     }
 
     printf("\n");
 
-    for (j = 4; j < 14; j++) {
+    for (j = 0; j < 10; j++) {
         printf("%3d | ", c);
 
         for (i = 0; i < 8; i++) {
-            printf("%2c ", c);
-            c++;
+            printf("%2c ", c++);
         }
 
         printf("\n");
     }
         printf("%3d | ", c);
 
-            for (i = 0; i < 7; i++) {
+        for (i = 0; i < 7; i++) {
             printf("%2c ", c);
             c++;
         }
 
-        printf("\n");
+        printf("   \n");
 
     return 0;
 }
