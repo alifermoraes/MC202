bb0e8a3df17bbe9d22c0f7dbca07bd8f12d961db
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-15 18:19:02 -0300

    Modified lab51-barras/main.c in order to improve memory consumption (not finished yet)

--- Labs/lab51-barras/main.c
+++ Labs/lab51-barras/main.c
@@ -9,33 +9,67 @@
  * Escreva um programa em C para imprimir um gráfico de frequências na tela.
  *
  * Entrada e saída
- * O programa recebe como entrada uma seqüência de triplas (número,freqencia,caractere). Para cada seqüência ele deve imprimir uma barra, produzindo um gráfico
+ * O programa recebe como entrada uma seqüência de triplas (número,freqencia
+ * caractere). Para cada seqüência ele deve imprimir uma barra, produzindo um
+ * gráfico
  * como no exemplo abaixo.
  *
- * Haverá pelo menos uma tripla na entrada, e todas são bem formadas. O número é menor que 1000 e a freqüência é maior ou igual a 1 e menor que 100.
+ * Haverá pelo menos uma tripla na entrada, e todas são bem formadas. O número
+ * é menor que 1000 e a freqüência é maior ou igual a 1 e menor que 100.
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 
+/* Estrutura para armazenar os valores da tripla ordenadamente */
 typedef struct {
-    int num;
-    int freq;
+    int number;
+    int frequency;
     char character;
 } Triple;
 
 typedef Triple * ptr_triple;
 
+/*
+ * Estrutura para armazenar as triplas em um vetor, seu tamanho e o numero de
+ * triplas armazenadas
+ */
+typedef struct {
+    ptr_triple* triples;
+    int size;
+    int index;
+}   Vector;
+
 int main(void) {
     int counter = 0;
     int i, j, num, freq;
-    char _char;
-    ptr_triple* triple_v;
+    char character;
+    ptr_triple* tmp = NULL;
 
-    triple_v = malloc(1000 * sizeof(ptr_triple));
-
+    /* Inicia o vetor de triplas com capacidade para 1 tripla*/
+    Vector vector;
+    vector.triples_v = malloc(sizeof(Triple))
+    vector.size = 1;
+    vector.index = 0;
+    
+    /*
+     * Lê as entradas e as armazena no vetor de triplas.
+     */
     do {
-        triple_v[counter] = malloc(sizeof(Triple));
+        /* Se o vetor de triplas estiver completo, dobra seu tamanho */
+        if (vector.index = vector.size) {
+            tmp = vector.triples_v;
+            vector.triples_v = malloc((2 * vector.size) * sizeof(Triple));
+
+            for (i = 0; i < vector.size; i++) {
+                vector.triples_v[i] = tmp.triples_v[i];
+            }
+
+            vector.size *= 2;
+            free(tmp);
+            tmp = NULL;         
+        }
+        
         scanf(" (%d,%d,%c)", &triple_v[counter]->num, &triple_v[counter]->freq, &triple_v[counter]->character);
         counter++;
     } while (getchar() != EOF);
