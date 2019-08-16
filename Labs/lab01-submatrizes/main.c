02e9044721b9f4ffb5799b3bf0702d5d603b4d88
Alifer Moraes <alifer.wsdm@gmail.com>
2019-08-16 08:46:06 -0300

    Fix some identation in lab01-submatrizes/main.c

--- Labs/lab01-submatrizes/main.c
+++ Labs/lab01-submatrizes/main.c
@@ -8,19 +8,17 @@
  * 
  * Submatriz
  * Dada uma matriz de inteiros positivos n por m, estamos interessados em
- * encontrar a maior soma de
- * elementos possível em uma submatriz de tamanho r por s.
+ * encontrar a maior soma de elementos possível em uma submatriz de tamanho r
+ * por s.
  *
  * Entrada
  * A entrada tem uma linha com inteiros n, m, r, s separados por espaços, sendo
  * que 1≤n≤1000, 1≤m≤1000, 1≤r≤n e 1≤s≤m. Depois vêm n linhas com m inteiros
- * cada, com os números em cada linha
- * e coluna da matriz.
+ * cada, com os números em cada linha e coluna da matriz.
  *
  * Saída
  * O programa deve imprimir o valor da maior soma possível em uma submatriz de
- * tamanho r por s em
- * uma única linha.
+ * tamanho r por s em uma única linha.
  */
 
 #include <stdio.h>
