932b055e125e5623caadbc1d30502536996a9956
Alifer Moraes <alifer.wsdm@gmail.com>
2019-11-10 23:41:54 -0300

    Add minor changes in code style

--- labs/lab12-heapsort/main.c
+++ labs/lab12-heapsort/main.c
@@ -26,13 +26,11 @@ int main(void) {
     heap = heap_create(max_size);
 
     if (max_size > 0) {
-        for (i = 0; i < max_size; i++) {
+        for (i = 0; i < max_size; i++)
             heap_remove_max(heap);
-        }
 
-        for (i = 0; i < max_size; i++) {
+        for (i = 0; i < max_size; i++)
             printf("%d ", heap->V[i]);
-        }
     }
 
     printf("\n");
